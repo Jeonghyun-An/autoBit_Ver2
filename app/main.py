3f3217bb721079336b0c6ea64167d9367654a94d
Hyonii <131145818+Jeonghyun-An@users.noreply.github.com>
2025-08-04 15:42:18 +0900

    [feat] 프론트 연결 용 설정 추가

--- app/main.py
+++ app/main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
 from app.routers import trade, model, strategy
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Bitconin AI AutoTrader")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Nuxt dev 서버
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(trade.router, prefix="/trade", tags=["Trade"])
 app.include_router(model.router, prefix="/model", tags=["Model"])
 app.include_router(strategy.router, prefix="/strategy", tags=["Strategy"])
+
+
+
